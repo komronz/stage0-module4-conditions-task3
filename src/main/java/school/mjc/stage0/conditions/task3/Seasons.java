5e71f36872313c3859d17e4e3a55410cf6f43299
komronz <pandacoder0820@gmail.com>
2022-07-30 11:05:34 +0500

    module4task3

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,17 +2,45 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-        if(month >0 && month < 13){
-            if(month> 10 || month< 2 ){
+        if (month < 13){
+            if (month == 1){
                 System.out.println("Winter");
-            } else if (month <6) {
+            }
+            if (month == 2){
+                System.out.println("Winter");
+            }
+            if (month == 3){
                 System.out.println("Spring");
-            }else if (month < 8){
+            }
+            if (month == 4){
+                System.out.println("Spring");
+            }
+            if (month == 5){
+                System.out.println("Spring");
+            }
+            if (month == 6){
                 System.out.println("Summer");
-            }else {
+            }
+            if (month == 7){
+                System.out.println("Summer");
+            }
+            if (month == 8){
+                System.out.println("Summer");
+            }
+            if (month == 9){
                 System.out.println("Autumn");
             }
-        }else {
+            if (month == 10){
+                System.out.println("Autumn");
+            }
+            if (month == 11){
+                System.out.println("Autumn");
+            }
+            if (month == 12){
+                System.out.println("Winter");
+            }
+        }
+        else {
             System.out.println("wrong number!");
         }
     }
