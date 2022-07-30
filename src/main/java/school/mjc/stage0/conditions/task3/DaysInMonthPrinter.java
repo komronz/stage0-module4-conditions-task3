9746321334107e1d12ee6e1b97bd8e3f4d561759
komronz <pandacoder0820@gmail.com>
2022-07-30 11:01:57 +0500

    module4task3

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -1,6 +1,12 @@
 package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
+    int days[] = {31,28,31,30,31,30,31,31,30,31,30,31};
     public void amountOfDays(int month) {
+        if(month> 0 && month < 13){
+            System.out.println(days[month-1]);
+        }else {
+            System.out.println("wrong number!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,24 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.util.Scanner;
+
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double answer = 0.0;
+        if (0<salary){
+            System.out.println("wrong input!");
+        }
+        else if (salary<=10_000){
+            answer = salary-(salary * 15.0 /100);
+            System.out.println(answer);
+        }
+        else if (salary<=20_000){
+            answer = salary-(salary*18.0/100);
+            System.out.println(answer);
+        }
+        else if (salary>20_000){
+            answer = salary - (salary*20.0/100);
+            System.out.println(answer);
+        }
     }
 }
